99baf9db99e5bb1dbe3d5de9a2c08b945c930b25
jisu <dlawotn3@naver.com>
2024-09-07 11:34:30 +0900

    feat: add BaseEntity class

---
+++ common/src/main/kotlin/com/bibbi/domain/BaseEntity.kt
@@ -0,0 +1,26 @@
+package com.bibbi.domain
+
+import jakarta.persistence.Column
+import jakarta.persistence.EntityListeners
+import jakarta.persistence.MappedSuperclass
+import lombok.Getter
+import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedDate
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
+import java.time.LocalDateTime
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
+@Getter
+open class BaseEntity {
+
+    @CreatedDate
+    @Column(name = "created_at", nullable = false, updatable = false)
+    var createdAt: LocalDateTime? = null
+        private set
+
+    @LastModifiedDate
+    @Column(name = "updated_at", nullable = false)
+    var updatedAt: LocalDateTime? = null
+        private set
+}
