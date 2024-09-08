811a8d6a320c02038df033c063f1d2dd453a3109
Kwon770 <sckwon770@gmail.com>
2024-09-08 16:40:47 +0900

    refactor: Change the type of topPercentage field of TopPercentageHistory model as Var from Val

--- family/src/main/kotlin/com/bibbi/domain/model/TopPercentageHistory.kt
+++ family/src/main/kotlin/com/bibbi/domain/model/TopPercentageHistory.kt
@@ -5,7 +5,7 @@ import java.time.LocalDateTime
 data class TopPercentageHistory(
     val topPercentageHistoryId: TopPercentageHistoryId,
     val family: Family,
-    val topPercentage: Int,
+    var topPercentage: Int,
     val createdAt : LocalDateTime?,
     val updatedAt : LocalDateTime?,
 )
