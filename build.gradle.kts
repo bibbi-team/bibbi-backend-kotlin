fb26146a5bbb598d86601763429bca22230fabea
jisu <dlawotn3@naver.com>
2024-09-15 17:27:51 +0900

    chore: add kotlin-reflect dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,6 +58,7 @@ subprojects {
 		implementation("org.springframework.boot:spring-boot-starter-security")
 		implementation("com.github.f4b6a3:ulid-creator:5.2.2")
 		implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+		implementation("org.jetbrains.kotlin:kotlin-reflect")
 		runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
 		runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 		compileOnly("org.projectlombok:lombok")
