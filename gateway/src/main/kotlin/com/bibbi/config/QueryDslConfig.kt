22cc67ddcd56611ce9df84edc589d8a307a31d2c
Jisu Lim <69844138+Ji-soo708@users.noreply.github.com>
2024-09-24 22:47:30 +0900

    [OING-363] chore: QueryDSL 의존성 및 설정 파일 추가 (#8)

---
+++ gateway/src/main/kotlin/com/bibbi/config/QueryDslConfig.kt
@@ -0,0 +1,18 @@
+package com.bibbi.config
+
+import com.querydsl.jpa.impl.JPAQueryFactory
+import jakarta.persistence.EntityManager
+import jakarta.persistence.PersistenceContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class QueryDslConfig {
+    @PersistenceContext
+    lateinit var entityManager: EntityManager
+
+    @Bean
+    fun jpaQueryFactory(): JPAQueryFactory {
+        return JPAQueryFactory(entityManager)
+    }
+}
